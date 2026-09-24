8777b045af7a1c7f68a21c1c8ec1032e19ef0d6f
Ravomahandry <ravomahandry@gmail.com>
2026-09-24 11:48:28 +0300

    Ajout du gitignore et suppression des fichiers de build

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_io_arvo_dataconso_ArvoApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "io.arvo.dataconso.ArvoApplication"
-)
-public final class _io_arvo_dataconso_ArvoApplication {
-}
--- app/build/intermediates/cxx/Debug/5t701o40/logs/x86/build_command_arvo_native.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Ravo\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\LifeBook\\Arvo\\app\\.cxx\\Debug\\5t701o40\\x86" ^
-  arvo_native
